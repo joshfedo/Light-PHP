5d5b6e8514c4a552c59a69cf737cd6cf89776cf5
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-02-28 21:14:57 +0100

    Change folders

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 require('Config.php');
 
 //Engine
-require(BACK_SYSTEM . 'core/Settings.php');
-require(BACK_SYSTEM . 'core/Url.php');
-require(BACK_SYSTEM . 'core/Controller.php');
-//require(BACK_SYSTEM . 'core/Session.php');
-//require(BACK_SYSTEM . 'core/Security.php');
-require(BACK_SYSTEM . "core/Output.php");
-require(BACK_SYSTEM . 'core/Connection.php');
-require(BACK_SYSTEM . 'core/Util.php');
-require(BACK_SYSTEM . 'core/ErrorManagent.php');
-require(BACK_SYSTEM . 'core/Loader.php');
+require(BACK_SYSTEM . 'engine/Settings.php');
+require(BACK_SYSTEM . 'engine/Url.php');
+require(BACK_SYSTEM . 'engine/Controller.php');
+//require(BACK_SYSTEM . 'engine/Session.php');
+//require(BACK_SYSTEM . 'engine/Security.php');
+require(BACK_SYSTEM . "engine/Output.php");
+require(BACK_SYSTEM . 'engine/Connection.php');
+require(BACK_SYSTEM . 'engine/Util.php');
+require(BACK_SYSTEM . 'engine/ErrorManagent.php');
+require(BACK_SYSTEM . 'engine/Loader.php');
 
 require(BACK_SYSTEM. "Start.php");
 
